8bcc192fe3ebab84122ed0ce72a36baecd5856b8
sgtkingo <33989823+sgtkingo@users.noreply.github.com>
2019-10-16 16:39:00 +0200

    upd_161019_DONE

--- I2CLibV2.h
+++ I2CLibV2.h
@@ -36,18 +36,55 @@
 #define SCL PORTCbits.RC3 //SCL init as 0
 #define SDA PORTCbits.RC4 //SDA init as 0
 
-#define F_OSC 16000000                        /* define F_OSC for finding bit rate*/
-#define I2C_CLOCK  100000                    /* I2C clock frequency is 100 kHz  */
-#define BITRATE ((F_OSC/(4*I2C_CLOCK))-1)    /* find bit rate to assign this value to SSPADD register */
+#define WBIT 0b11111110;
+#define RBIT 0b11111111;
 
-#define _XTAL_FREQ 16000000 
+#define I2C_CLOCK  100000                    /* I2C clock frequency is 100 kHz  */
+
+/* find bit rate to assign this value to SSPADD register, _XTAL_FREQ=FOSH */
+#define BITRATE ((_XTAL_FREQ/(4*I2C_CLOCK))-1)
+
+bit I2CError=false;
+//Function DECLARATION:
+void I2C_CLEARBUS();
+void I2C_CLEARBUFFER();
+void I2C_IDLE();
+void MSSP_WAIT();
+void I2C_INIT();
+void I2C_STR();
+void I2C_STP();
+void WriteI2C( unsigned char data_out );
+unsigned char I2CRead(char mode);
+
+
+//Function DEFINITION:
+void I2C_CLEARBUS(){   
+    SSP1CON1bits.SSPEN = 0;   // Disable I2C operation
+    I2C_CLEARBUFFER();
+    
+    TRISCbits.RC3=0; //SCL as OUT
+    TRISCbits.RC4=0; //SDA as OUT
+    SDA=SCL=0;
+    TRISCbits.RC3=1; //SCL as IN
+    TRISCbits.RC4=1; //SDA as IN
+    
+    //Check if bus still running
+    if(!(SCL | SDA)){
+        //stop if runnig
+        I2C_STP();
+    }
+}
+
+void I2C_CLEARBUFFER(){   
+    SSP1BUF=0x00;
+    PIR1bits.SSPIF=0;
+}
+
 
 void I2C_IDLE()
 {
- //while ( (SSP1CON1 & 0b00011111) || (SSP1STAT & 0b00000100) ) ; 
-while ( (SSP1STAT & 0b00000100) ) ; 
- //check the this on registers to make sure the                                                                 
- //IIC is not in progress
+//check if I2C is in progress
+while ( (SSP1STATbits.READ_WRITE | (SSP1CON2bits.SEN | SSP1CON2bits.PEN)) ) ; 
 }
 
 void MSSP_WAIT()
@@ -56,43 +93,56 @@ void MSSP_WAIT()
     PIR1bits.SSPIF=0 ;
 }
 
-void I2C_Init()
+void I2C_INIT()
 {
         ANSELCbits.ANSC3=0; //SCL as dig 
         ANSELCbits.ANSC4=0; //SDA as dig
         TRISCbits.RC3=1; //SCL as IN
         TRISCbits.RC4=1; //SDA as IN
         SDA=SCL=0;
-        SSP1STAT = 0b00000000;           /* slew rate disabled, other bits are cleared*/
-        SSP1CON1  = 0x28;     /* enable SSP port for I2C Master mode,
+        
+        SSP1STAT = 0x00;      /* slew rate disabled, other bits are cleared*/
+        SSP1CON1 = 0x28;     /* enable SSP port for I2C Master mode,
                                          * clock = FOSC / (4 * (SSPADD+1)) */
         SSP1ADD  = BITRATE;              /* clock 100 kHz */  
+        
         INTCONbits.GIE=1;
         INTCONbits.PEIE=1;
         PIE1bits.SSPIE = 1;             /* enable SSPIF interrupt */
         PIR1bits.SSPIF = 0;             
 }
 
-void I2C_STAT()
+void I2C_STR()
 { 
-    //SSP1STATbits.S = 1; // Initialize the start condition 
-    SSP1CON2bits.SEN=1;
+    SSP1CON1bits.SSPEN = 1;   // Enable I2C operation
+    SSP1CON2bits.SEN=1; // Initialize the start condition
     I2C_IDLE();        // check if the I2C bus is idle
     MSSP_WAIT();       // wait till completion of event
 }
+
+void I2C_STP()
+{
+    SSP1CON2bits.PEN = 1;      // Initiate stop condition
+    //MSSP_WAIT();            // Wait till completion of event
+    I2C_CLEARBUFFER();
+    I2C_IDLE();
+    SSP1CON1bits.SSPEN = 0;   // Disable I2C operation
+}
+
 
 void WriteI2C( unsigned char data_out )
 {
     SSP1BUF = data_out;           // write single byte to SSPBUF
     MSSP_WAIT();
+    I2CError=SSP1CON2bits.ACKSTAT;
 }
 
 
-unsigned char I2CRead()
+unsigned char I2CRead(char mode)
 {
     char data=0;
     SSP1CON2bits.RCEN=1; //recieve
-    SSP1CON2bits.ACKDT=1; //no ACK
+    SSP1CON2bits.ACKDT=!(mode & 0x01); //MODE TRUE=ACK, MODE FALSE=NACK
     
     MSSP_WAIT();
     data=SSP1BUF; //read data
@@ -100,13 +150,6 @@ unsigned char I2CRead()
     SSP1CON2bits.ACKEN=1;//ack sequence
     
     return data;        // Data from eeprom is now in the SSPBUF so return that value
-}
-
-void I2C_STP()
-{
-    SSP1CON2bits.PEN = 1;      // Initiate stop condition
-    MSSP_WAIT();            // Wait till completion of event
-    SSP1CON1bits.SSPEN = 0;   // Disable I2C operation
 }
 
 /*void main()
